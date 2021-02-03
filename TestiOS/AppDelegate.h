b52907aff9b338e3944540adfdb9423f314ce53a
Ayush Khare <ayush@healint.com>
2021-02-03 14:54:52 +0800

    Initial Commit

---
+++ TestiOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestiOS
+//
+//  Created by Ayush Khare on 3/2/21.
+//  Copyright © 2021 Ayush Khare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
